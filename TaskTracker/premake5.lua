06ecdb2b68a474516cc5382bb7d65f28470b48b4
Shaun Broomfield <36537416+Enstroe@users.noreply.github.com>
2022-08-15 22:53:51 +0100

    init
    Initial Setup

--- TaskTracker/premake5.lua
+++ TaskTracker/premake5.lua
@@ -1,4 +1,4 @@
-project "WalnutApp"
+project "TaskTracker"
    kind "ConsoleApp"
    language "C++"
    cppdialect "C++17"
--- TaskTracker/src/TaskTracker.cpp
+++ TaskTracker/src/TaskTracker.cpp
@@ -19,7 +19,7 @@ public:
 Walnut::Application* Walnut::CreateApplication(int argc, char** argv)
 {
 	Walnut::ApplicationSpecification spec;
-	spec.Name = "Walnut Example";
+	spec.Name = "Task Tracker";
 
 	Walnut::Application* app = new Walnut::Application(spec);
 	app->PushLayer<ExampleLayer>();
